a87726a79d4ee53b2d13000fc63ab89184eca3b7
L1n <490772448@qq.com>
2016-08-07 22:00:38 +0800

    书里没有自动化配置 nginx 和 gunicorn 的脚本, 自己编写了一个

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -24,7 +24,7 @@ def deploy():
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
-    _set_nginx_gunicron(env.host)
+    _set_nginx_gunicron(source_folder, env.host)
 
 
 def _create_directory_structure_if_necessary(site_folder):
@@ -90,23 +90,25 @@ def _update_database(source_folder):
     run("cd {} && ../../virtualenv/bin/python3 manage.py migrate --noinput".format(source_folder + "/todo_app/"))
 
 
-def _set_nginx_gunicron(site_name):
+def _set_nginx_gunicron(source_folder, site_name):
     """
     自己写的, 利用 sed 来配置 nginx 以及 gunicorn
+    :param source_folder: 文件夹路径
     :param site_name: 网站名, 比如 "watch0.top"
     :return:
     """
     # 这里，使用 s/replaceme/withthis/g 句法把字符串 SITENAME 替换成网站的地址。
     # 然后使用管道操作（|）把文本流传给一个有 root 权限的用户处理（sudo），把传入的文本流写入一个文件
     # 即 sites-available 文件夹中的一个虚拟主机配置文件。
-    run('sed "s/todo_app/{host}/g" nginx.template.conf | sudo tee /etc/nginx/sites-available/{host}'
-        .format(host=site_name))
+    run(
+        'cd {} && sed "s/todo_app/{host}/g" deploy_tools/nginx.template.conf | sudo tee /etc/nginx/sites-available/{host}'
+        .format(source_folder, host=site_name))
 
     # 激活这个文件配置的虚拟主机
     run('sudo ln -s ../sites-available/{host} /etc/nginx/sites-enabled/{host}'.format(host=site_name))
 
     # 编写 Upstart 脚本
-    run('sed "s/SITENAME/{host}/g" gunicorn-upstart.template.conf | sudo tee /etc/init/gunicorn-{host}'
+    run('sed "s/SITENAME/{host}/g" deploy_tools/gunicorn-upstart.template.conf | sudo tee /etc/init/gunicorn-{host}'
         .format(host=site_name))
 
     # 最后，启动这两个服务
