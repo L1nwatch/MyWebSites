766a65d7b1787187989b47b9593f40bf2bfea229
L1n <490772448@qq.com>
2016-08-27 09:21:31 +0800

    为登录编写功能测试

---
+++ todo_app/functional_tests/test_login.py
@@ -0,0 +1,53 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+""" Description
+"""
+# functional_tests/test_login.py
+from base import FunctionalTest
+import time
+
+__author__ = '__L1n__w@tch'
+
+
+class LoginTest(FunctionalTest):
+    def test_login_with_persona(self):
+        # Y 访问这个很棒的超级列表网站
+        # 第一次注意到 "Sign in" 链接
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('login').click()
+
+        # 出现一个 Persona 登录框
+        # 需要辅助函数，它们都用于实现 Selenium 测试中十分常见的操作：等待某件事发生。
+        self.switch_to_new_window("Mozilla Persona")
+
+        # Y 使用她的电子邮件地址登录
+        ## 测试中的电子邮件使用 mockmyid.com
+        # 可以使用如下方法查找 Persona 电子邮件输入框的 ID：手动打开网站，使用 Firefox 调试工具条(`Ctrl + Shift + I`)
+        # 这里没有使用真实的电子邮件地址，而是用虚拟工具生成的地址，因此不用在邮件服务供应商的网站上填写认证信息。虚拟工具可以使用 MockMyID 或者 Persona Test User
+        self.browser.find_element_by_id("authentication_email").send_keys("edith@mockmyid.com")
+        self.browser.find_element_by_tag_name("button").click()
+
+        # Persona 窗口关闭
+        self.switch_to_new_window("To-Do")
+
+        # 她发现自己已经登录
+        # 需要辅助函数，它们都用于实现 Selenium 测试中十分常见的操作：等待某件事发生。
+        self.wait_for_element_with_id("logout")
+        navbar = self.browser.find_element_by_css_selector(".navbar")
+        self.assertIn("edith@mockmyid.com", navbar.text)
+
+    def switch_to_new_window(self, text_in_title):
+        retries = 60
+        while retries > 0:
+            for handle in self.browser.window_handles:
+                self.browser.switch_to_window(handle)
+                if text_in_title in self.browser.title:
+                    return
+            retries -= 1
+            time.sleep(0.5)
+        self.fail("could not find window")
+
+
+if __name__ == "__main__":
+    pass
