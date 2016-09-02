2701f55da8fd0938fc1fd11cdd24f52cfd7b09a6
L1n <490772448@qq.com>
2016-09-02 22:46:54 +0800

    单元测试通过了, 但只实现了点击时才调用 request

---
+++ todo_app/accounts/static/accounts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by L1n on 16/9/2.
+ */
+var initialize = function (navigator) {
+    navigator.id.request();
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
