f9e6f2d1c8b2b725df7f30894e397054ae92eb5a
L1n <490772448@qq.com>
2016-09-05 22:28:11 +0800

    跳过认证的实现,已经通过了功能测试了

--- todo_app/functional_tests/test_login.py
+++ todo_app/functional_tests/test_login.py
@@ -7,9 +7,10 @@
 from unittest import skip
 from base import FunctionalTest
 import time
-from selenium.webdriver.support.ui import WebDriverWait
 
 __author__ = '__L1n__w@tch'
+
+TEST_EMAIL = "edith@mockmyid.com"
 
 
 class LoginTest(FunctionalTest):
@@ -27,7 +28,7 @@ class LoginTest(FunctionalTest):
         ## 测试中的电子邮件使用 mockmyid.com
         # 可以使用如下方法查找 Persona 电子邮件输入框的 ID：手动打开网站，使用 Firefox 调试工具条(`Ctrl + Shift + I`)
         # 这里没有使用真实的电子邮件地址，而是用虚拟工具生成的地址，因此不用在邮件服务供应商的网站上填写认证信息。虚拟工具可以使用 MockMyID 或者 Persona Test User
-        self.browser.find_element_by_id("authentication_email").send_keys("edith@mockmyid.com")
+        self.browser.find_element_by_id("authentication_email").send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name("button").click()
 
         # Persona 窗口关闭
@@ -35,20 +36,20 @@ class LoginTest(FunctionalTest):
 
         # 她发现自己已经登录
         # 需要辅助函数，它们都用于实现 Selenium 测试中十分常见的操作：等待某件事发生。
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(TEST_EMAIL)
 
         # 刷新页面，她发现真的通过会话登录了
         # 而且并不只在那个页面中有效
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(TEST_EMAIL)
 
         # 对这项新功能有些恐惧，她立马点击了退出按钮
         self.browser.find_element_by_id("id_logout").click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(TEST_EMAIL)
 
         # 刷新后仍旧保持退出状态
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(TEST_EMAIL)
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
@@ -60,21 +61,6 @@ class LoginTest(FunctionalTest):
             retries -= 1
             time.sleep(0.5)
         self.fail("could not find window")
-
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id), "Could not find element with id {}. Page text was {}"
-                .format(element_id, self.browser.find_element_by_tag_name("body").text))
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id("id_logout")
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertIn("edith@mockmyid.com", navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id("id_login")
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertNotIn("edith@mockmyid.com", navbar.text)
 
 
 if __name__ == "__main__":
---
+++ todo_app/functional_tests/test_my_lists.py
@@ -0,0 +1,50 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+"""
+跳过认证, 进行功能测试
+"""
+from django.conf import settings
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.contrib.sessions.backends.db import SessionStore
+from base import FunctionalTest
+
+__author__ = '__L1n__w@tch'
+
+User = get_user_model()
+
+
+class MyListsTest(FunctionalTest):
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        # 在数据库中创建一个会话对象。会话键的值是用户对象的主键，即用户的电子邮件地址
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+
+        ## 为了设定 cookie，我们要先访问网站
+        ## 而 404 页面是加载最快的
+        self.browser.get(self.server_url + "/404_no_such_url/")
+        # 然后把一个 cookie 添加到浏览器中，cookie 的值和服务器中的会话匹配。这样再次访问网站时，服务器就能识别已登录的用户。
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path="/",
+        ))
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = "edith@example.com"
+
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_out(email)
+
+        # Y 是已登录用户
+        self.create_pre_authenticated_session(email)
+
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_in(email)
+
+
+if __name__ == "__main__":
+    pass
