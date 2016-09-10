f12eb10dc69dd92d28350029b19542eff70ee0fa
L1n <490772448@qq.com>
2016-09-10 21:06:21 +0800

    测试一下为什么远程管理数据库会失败

--- todo_app/functional_tests/test_my_lists.py
+++ todo_app/functional_tests/test_my_lists.py
@@ -5,6 +5,7 @@
 跳过认证, 进行功能测试
 """
 from unittest import skip
+import time
 from server_tools import create_session_on_server
 from base import FunctionalTest
 from management.commands.create_session import create_pre_authenticated_session
@@ -28,16 +29,17 @@ class MyListsTest(FunctionalTest):
         ## 为了设定 cookie，我们要先访问网站
         ## 而 404 页面是加载最快的
         self.browser.get(self.server_url + "/404_no_such_url/")
-        print("即将添加的 cookie 值, name={}, value={}".format(settings.SESSION_COOKIE_NAME, str(session_key.splitlines()[0])))
+        print(
+            "即将添加的 cookie 值, name={}, value={}".format(settings.SESSION_COOKIE_NAME, str(session_key.splitlines()[0])))
         print("服务器创建的 cookie = {}".format(session_key))
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
-            value=str(session_key.splitlines()[0]),
+            value=session_key.splitlines()[0].decode().strip(),
             path="/",
         ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = "490772448@qq.com"
+        email = "edith@mockmyid.com"
 
         self.browser.get(self.server_url)
         self.wait_to_be_logged_out(email)
@@ -46,6 +48,7 @@ class MyListsTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
 
         self.browser.get(self.server_url)
+        time.sleep(300)
         self.wait_to_be_logged_in(email)
 
 
