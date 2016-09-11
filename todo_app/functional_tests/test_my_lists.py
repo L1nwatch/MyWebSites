675bea3c6ae76e9f22299b7111405bfab9b03e52
L1n <490772448@qq.com>
2016-09-11 10:22:46 +0800

    编写完功能测试准备开始开发了

--- todo_app/functional_tests/test_my_lists.py
+++ todo_app/functional_tests/test_my_lists.py
@@ -47,6 +47,48 @@ class MyListsTest(FunctionalTest):
         self.browser.get(self.server_url)
         self.wait_to_be_logged_in(email)
 
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        # Y 是已登录用户
+        self.create_pre_authenticated_session("edith@mockmyid.com")
+
+        # 她访问首页，新建一个清单
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys("Reticulate splines\n")
+        self.get_item_input_box().send_keys("Immanentize eschaton\n")
+        first_list_url = self.browser.current_url
+
+        # 她第一次看到 My Lists 链接
+        self.browser.find_element_by_link_text("My Lists").click()
+
+        # 她看到这个页面中有她创建的清单
+        # 而且清单根据第一个待办事项命名
+        self.browser.find_element_by_link_text("Reticulate splines").click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # 她决定再建一个清单试试
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys("Click cows\n")
+        second_list_url = self.browser.current_url
+
+        # 在 My Lists 页面，这个新建的清单也显示出来了
+        self.browser.find_element_by_link_text("My Lists").click()
+        self.browser.find_element_by_link_text("Click cows").click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # 她决定再建一个清单试试
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys("Click cows\n")
+        second_list_url = self.browser.current_url
+
+        # 在 My Lists 页面，这个新增的清单也显示出来了
+        self.browser.find_element_by_link_text("My Lists").click()
+        self.browser.find_element_by_link_text("CLick cows").click()
+        self.assertEqual(self.browser.current_url, second_list_url)
+
+        # 她退出后, My Lists 链接不见了
+        self.browser.find_element_by_id("id_logout").click()
+        self.assertEqual(self.browser.find_elements_by_link_text("My Lists"), [])
+
 
 if __name__ == "__main__":
     pass
