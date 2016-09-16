f986dccbaf1d9f58c520bf56fa1bf59116c0671a
L1n <490772448@qq.com>
2016-09-16 10:14:12 +0800

    隔离,单元,功能测试等全部通过了

--- todo_app/lists/tests/test_models.py
+++ todo_app/lists/tests/test_models.py
@@ -4,9 +4,10 @@
 """
 这个文件只包含模型测试
 """
-from django.test import TestCase
 from lists.models import Item, List
+
 from django.core.exceptions import ValidationError
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 __author__ = '__L1n__w@tch'
@@ -19,13 +20,29 @@ class ListModelTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), "/lists/{}/".format(list_.id))
 
+    def test_create_new_creates_list_and_first_item(self):
+        List.create_new(first_item_text="new item text")
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "new item text")
+        new_list = List.objects.first()
+        self.assertEqual(new_item.list_attr, new_list)
+
+    def test_create_new_optionally_saves_owner(self):
+        user = User.objects.create()
+        List.create_new(first_item_text="new item text", owner=user)
+        new_list = List.objects.first()
+        self.assertEqual(new_list.owner, user)
+
     def test_lists_can_have_owners(self):
-        user = User.objects.create(email="a@b.com")
-        list_ = List.objects.create(owner=user)
-        self.assertIn(list_, user.list_set.all())
+        List(owner=User())  # 不该抛出异常
 
     def test_list_owner_is_optional(self):
-        List.objects.create()  # 不该抛出异常
+        List().full_clean()  # 不该抛出异常
+
+    def test_create_returns_new_list_object(self):
+        returned = List.create_new(first_item_text="new item text")
+        new_list = List.objects.first()
+        self.assertEqual(returned, new_list)
 
     def test_list_name_is_first_item_text(self):
         list_ = List.objects.create()
